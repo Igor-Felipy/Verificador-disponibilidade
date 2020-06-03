07f2db69669bb07f3164d6a4877ead4f0ff7ba36
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-03 17:18:32 -0400

    adicionando tipos de respostas

--- verificador/main.py
+++ verificador/main.py
@@ -2,10 +2,7 @@ import verificar
 
 url = input('qual site verificar? ')
 
+
+
 response = verificar.verify(url)
-if response == 200:
-    print('Site no ar')
-elif response == 404:
-    print('Site não encontrado! ')
-elif response == 400:
-    ('Site não funcionando!')
+
--- verificador/verificar.py
+++ verificador/verificar.py
@@ -2,7 +2,35 @@ import requests
 
 def verify(url):
     url = url
+    status = requests.get(url)
+    return status.status_code
 
-    status = requests.get(url).status_code()
 
-    return status
+def get_response(status):
+    status = status
+    while True:
+        if status == 100:
+            print('The server want more time of connection')
+        elif status == 101:
+            print('The server want switch a protocol')
+        elif status == 102:
+            print('The server is processing the information')
+        elif status == 103:
+            print('The server want to pre-charge resources')
+        elif status == 200:
+            print('Everything is OK')
+        elif status == 201:
+            print('Everything is OK and a resource was succesfully create')
+        elif status == 200:
+            print('')
+        elif status == 202:
+            print('The request was received but not ')
+        elif status == 404:
+            print('')
+        elif status == 400:
+            print('')
+
+
+def url_clean():
+    url = url
+    
