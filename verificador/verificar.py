7bf412c1582c327996221098b625a277a7172a47
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-03 17:41:14 -0400

    mais codigos de status https

--- verificador/verificar.py
+++ verificador/verificar.py
@@ -10,24 +10,84 @@ def get_response(status):
     status = status
     while True:
         if status == 100:
-            print('The server want more time of connection')
+            print('Continue')
         elif status == 101:
-            print('The server want switch a protocol')
+            print('Switching Protocol')
         elif status == 102:
-            print('The server is processing the information')
+            print('Processing (WebDAV)')
         elif status == 103:
-            print('The server want to pre-charge resources')
+            print('Early hints')
         elif status == 200:
-            print('Everything is OK')
+            print('OK')
         elif status == 201:
-            print('Everything is OK and a resource was succesfully create')
-        elif status == 200:
-            print('')
+            print('Created')
         elif status == 202:
-            print('The request was received but not ')
-        elif status == 404:
-            print('')
+            print('Accepted')
+        elif status == 203:
+            print('Non-Authoritative Information')
+        elif status == 204:
+            print('No Content')
+        elif status == 205:
+            print('Reset Content')
+        elif status == 206:
+            print('Partitial content')
+        elif status == 207:
+            print('Multi-Status (WebDAV)')
+        elif status == 208:
+            print('Multi-Status (WebDAV)')
+        elif status == 226:
+            print('IM Used (HTTP Delta encoding)')
+        elif status == 300:
+            print('Mutiple Choice')
+        elif status == 301:
+            print('Moved Permanently')
+        elif status == 302:
+            print('Found')
+        elif status == 303:
+            print('See Other')
+        elif status == 304:
+            print('Not Modified')
+        elif status == 305:
+            print('Use Proxy')
+        elif status == 306:
+            print('Unused')
+        elif status == 307:
+            print('Temporary Redirect')
+        elif status == 308:
+            print('Permanent Redirect')
         elif status == 400:
+            print('Bad request')
+        elif status == 401:
+            print('Unauthorized')
+        elif status == 402:
+            print('Payment Required')
+        elif status == 403:
+            print('Forbidden')
+        elif status == 404:
+            print('Not Found')
+        elif status == 405:
+            print('Method Not Allowed')
+        elif status == 406:
+            print('Not Acceptable')
+        elif status == 407:
+            print('Proxy Authentication')
+        elif status == 408:
+            print('Request Timeout')
+        elif status == 409:
+            print('Conflict')
+        elif status == 410:
+            print('Gone')
+        elif status == 411:
+            print('Length Required')
+        elif status == 412:
+            print('Precondition Failed')
+        elif status == 413:
+            print('Payload Too Large')
+        elif status == 414:
+            print('URI Too Long')
+        elif status == 415:
+            print('Unsupported Media Type')
+        elif status == 416:
             print('')
 
 
