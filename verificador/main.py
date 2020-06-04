874d27b1a17ed6e632f90675bd002c50c88e3b8e
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-04 14:41:36 -0400

    criando novas opções

--- verificador/main.py
+++ verificador/main.py
@@ -1,7 +1,40 @@
+from time import sleep
 import verify
 
-try:
-    url = verify.url_clean(input('qual site verificar? '))
-    verify.verify(url)
-except:
-    print('Error')
+
+while True:
+    menu = str(input('Unique verification [U] \nContinuous verification [C] \nCheck and save to file [S] \nHelp [H] \nQuit [Q] \n'))
+
+    if menu.upper() == 'U':
+        try:
+            url = verify.url_clean(input('Which site to check? '))
+            verify.verify(url)
+        except:
+            print('Error')
+
+
+    elif menu.upper() == 'C':
+        url = verify.url_clean(input('Which site to check? '))
+        while True:
+            try:
+                verify.verify(url)
+            except:
+                print('Error')
+                break
+            sleep(1)
+            
+
+    elif menu.upper() == 'S':
+        print()
+
+
+    elif menu.upper() == 'H':
+        print()
+
+
+    elif menu.upper() == 'Q':
+        print('Byeee....')
+        break
+
+    else:
+        print('Invalid Option!!')
