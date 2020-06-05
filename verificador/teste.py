1e0d3f526f61b0e181fd0c7d99022a9d001d7ffc
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-05 16:59:57 -0400

    testando

--- verificador/teste.py
+++ verificador/teste.py
@@ -2,6 +2,6 @@ import verify
 url = 'google.com'
 
 print(verify.verify(url))
-with open('google.txt', 'a+') as archive:
+with open('google.txt','a+') as archive:
     for i in archive:
-        print(verify.verify(url))
+        archive.write(verify.verify(url))
